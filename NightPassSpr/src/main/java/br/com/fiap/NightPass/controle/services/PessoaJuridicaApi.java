d5ba5712fe484afc5a1db75c764e215642d17908
Alexandre José <lexduarte@yahoo.com.br>
2020-04-21 12:13:43 -0300

    versão preliminiar API JSON com Banco de Dados da FIAP

--- NightPassSpr/src/main/java/br/com/fiap/NightPass/controle/services/PessoaJuridicaApi.java
+++ NightPassSpr/src/main/java/br/com/fiap/NightPass/controle/services/PessoaJuridicaApi.java
@@ -1,18 +1,22 @@
 package br.com.fiap.NightPass.controle.services;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
-import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.Response.Status;
 
 import br.com.fiap.NightPass.Api.Dao.PJuridicaApiDAO;
@@ -23,6 +27,8 @@ import br.com.fiap.NightPassSpr.Entity.PJuridica;
 
 @Path("/pessoajuridicaapi")
 @PermitAll
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 public class PessoaJuridicaApi {
 
 	
@@ -50,7 +56,7 @@ public class PessoaJuridicaApi {
 	
 	  @GET
 	  @RolesAllowed("guest")
-	  @Produces(MediaType.APPLICATION_JSON)
+
 	  public List<PJuridica> buscarTodas() {
 	  
 	  return dao.listar();
@@ -60,7 +66,6 @@ public class PessoaJuridicaApi {
 	  @GET
 	  @RolesAllowed("guest")
 	  @Path("/{psjCodigo}")
-	  @Produces(MediaType.APPLICATION_JSON)
 	  public PJuridica buscaPorChave(@PathParam("psjCodigo") long psjCodigo) {
 		  
 		  try {
@@ -75,16 +80,27 @@ public class PessoaJuridicaApi {
 		  }
 		  
 	  }
-	
-	/*
-	 * @GET
-	 * 
-	 * @Produces(MediaType.APPLICATION_JSON) public PJuridica buscarPJ() {
-	 * 
-	 * return dao.buscar(84L);
-	 * 
-	 * }
-	 */
-	
+	  
+	  
+	  @RolesAllowed("user")
+	  @POST
+	  @Path("cadastrarNovaPJ")
+	  public Response cadastrarNovaPJ (PJuridica pJuridicaNova) {
+
+		  try {
+
+			  dao.cadastrar(pJuridicaNova);
+			  dao.commit();
+			  URI uri = UriBuilder.fromPath("/{psjCodigo}").build(pJuridicaNova.getPsjCodigo());
+			  return Response.created(uri).entity(pJuridicaNova).build();
+
+		  }catch(Exception e) {
+
+			  throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
+
+		  }
+
+	  }
+
 	
 }
